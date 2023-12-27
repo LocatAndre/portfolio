1554387f97a07b0064282bad6a606966c035241c
Andrea Locatelli <andrea.locatelli@beije.it>
2023-12-27 22:45:09 +0100

    migrate to astro 4 and threlte 7

--- src/components/space/Space.svelte
+++ src/components/space/Space.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-	import { T, useFrame } from '@threlte/core'
+	import { T, useFrame, useTask } from '@threlte/core'
 	import { BufferAttribute, BufferGeometry, PointsMaterial } from 'three'
 
 	const particlesGeometry = new BufferGeometry()
@@ -17,9 +17,9 @@
 	})
 
 	let rotation = { x: 0, y: 0 }
-	useFrame(({ clock }) => {
+	useTask((delta) => {
 		rotation.y += Math.PI * 0.0001
-		rotation.x += Math.sin(clock.getElapsedTime()) * Math.sin(Math.random() - 0.5) * 0.0001
+		rotation.x += Math.sin(delta) * Math.sin(Math.random() - 0.5) * 0.0001
 	})
 </script>
 
